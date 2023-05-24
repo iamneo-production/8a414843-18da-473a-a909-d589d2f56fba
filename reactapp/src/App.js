89deb1d55c5306c62d9ffa96b539d4120ee57d35
Thavamuthubaskaran05 <thavamuthubaskaran0515@gmail.com>
2023-05-24 18:57:37 +0000

    Jome Screen Changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { TextInput } from '@mantine/core';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CustomHeader from './header';
+import HomeScreen from './screens/homeScreen';
 
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<CustomHeader />} />
+      <Route path="/" element={<HomeScreen />} />
       </Routes>
     </BrowserRouter>
     
--- reactapp/src/header.js
+++ reactapp/src/header.js
@@ -5,24 +5,38 @@ import {
     Group,
     Button,
     Box,
+    TextInput,
+    Select,
+    Flex
 } from '@mantine/core';
+import Logo from './assests/hospitalLogo.svg'
 
 export default function CustomHeader() {
     return (
-        <Box>
-        <Header height={60} px="md">
-            <Group position="apart">
-                <Text fw={500}>Hospital</Text>
-                <div style={{display:'flex',}}>
-                <Text px="sm" fw={500}>Home</Text>
-                <Text px="sm" fw={500}>Services</Text>
-                <Text px="sm" fw={500}>About us</Text>
-                <Text px="sm" fw={500}>Contact us</Text>
-                    </div>
-                <Button>SignUp</Button>
+        <Box p="md"  sx={{position:'fixed',width:'100%',
+        // backgroundColor:'#ADD8E6',
+        zIndex:2}}>
+            {/* <Header height={60} px="md"> */}
+            <Flex direction="row" justify="space-between">
+                <div style={{ display: 'flex', }}>
+                    <Image width={20} height={20} src={Logo} />
+                    <Text pl="xs" fw={500}>MediLab Hospital</Text>
+                </div>
+                <div style={{ display: 'flex', }}>
+                    <Text px="sm" fw={500} style={{ cursor: 'pointer' }}>Home</Text>
+                    <Text px="sm" fw={500} style={{ cursor: 'pointer' }}>Services</Text>
+                    <Text px="sm" fw={500} style={{ cursor: 'pointer' }}>About us</Text>
+                    <Text px="sm" fw={500} style={{ cursor: 'pointer' }}>Contact us</Text>
+                </div>
+                <div style={{ display: 'flex', }}>
+                    <Button mr="md" size="xs" radius="10px" sx={{ background: "linear-gradient(#9083D5, #807593)" }} >SignIn</Button>
+                    <Button size="xs" radius="10px" style={{ background: "linear-gradient(#9083D5, #807593)" }}>SignUp</Button>
+                </div>
 
-            </Group>
-        </Header>
+            </Flex>
+
+            {/* </Header> */}
+
         </Box>
     )
 }
