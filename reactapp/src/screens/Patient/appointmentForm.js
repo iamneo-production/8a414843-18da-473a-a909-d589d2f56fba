0a370527ca38609287a62391f7a434aec7d9304a
Sanga Chakrabarty <sangyachakrabarty@gmail.com>
2023-07-21 21:37:55 +0530

    appointment frontend changed

--- reactapp/src/screens/Patient/appointmentForm.js
+++ reactapp/src/screens/Patient/appointmentForm.js
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Card,
   Button,
@@ -8,179 +7,149 @@ import {
   Col,
   Title,
   Container,
+  Modal,
 } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import { Textarea } from "@mantine/core";
-import { DatePickerInput } from "@mantine/dates";
 import { useState } from "react";
-
+import { DatePickerInput } from "@mantine/dates";
 import { TimeInput } from "@mantine/dates";
 import { IconCalendar, IconClock2 } from "@tabler/icons-react";
+import axios from "axios";
 
-export default function Appointment() {
-  const form = useForm({
-    initialValues: {
-      name: "",
-      mobile: "",
-      email: "",
-      dob: null,
-      gender: "",
-      appointmentDate: null,
-      time: "",
-      dept: "",
-      doctor: "",
-      address: "",
-    },
-    validate: {
-      name: (value) =>
-        value.length < 2 ? "Name should contain atleast two letters" : null,
-      mobile: (value) =>
-        value.length !== 10 ? "Mobile number should contain 10 digits" : null,
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      dob: (value) => (!value ? "Please select date of birth" : null),
-      gender: (value) => (value === "" ? "Choose Gender" : null),
-      date: (value) => (!value ? "Please select an appointment date" : null),
-      time: (value) => (value === "" ? "Choose Appointment Time" : null),
-      dept: (value) => (value === "" ? "Choose Department" : null),
-      doctor: (value) => (value === "" ? "Choose Doctor" : null),
-      address: (value) => (value.length < 1 ? "Address cannot be blank" : null),
-    },
+export default function ModalForm({ onCloseModal, patientId }) {
+  const [patient, setPatient] = useState({
+    issue: "",
+    doctorId: "",
+    date: null,
+    time: null,
+    patientId: patientId, // Set the patientId as the default value
   });
 
+  const { doctorId, date, time, issue } = patient;
+
+  const onInputChange = (e) => {
+    const { name, value } = e.target;
+    setPatient((prevPatient) => ({
+      ...prevPatient,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post("http://localhost:8080/api/appointment", patient);
+    onCloseModal();
+  };
+
   return (
-    <div
-      style={{
-        backgroundColor: "#CCCCCC",
-        paddingLeft: "150px",
-        paddingTop: "67px",
-        paddingBottom: "30px",
-      }}
-    >
-      <Card bg="white" radius="lg" m={30} px={10}>
-        <form onSubmit={form.onSubmit(console.log)}>
-          <Grid m={0} px={10}>
-            <Grid.Col xs={12} lg={12}>
-              <div style={{ display: "flex", color: "grey" }}>
-                <Title mb="md">Appointment</Title>
-              </div>
-            </Grid.Col>
-          </Grid>
-          <Grid pt="xs" m={0} px={40}>
-            <Grid.Col xs={6} lg={6}>
-              <TextInput
-                radius="md"
-                placeholder="Full Name"
-                {...form.getInputProps("name")}
-              />
-            </Grid.Col>
-            <Grid.Col xs={6} lg={6}>
-              <TextInput
-                radius="md"
-                placeholder="Mobile Number"
-                {...form.getInputProps("mobile")}
-              />
-            </Grid.Col>
+    <>
+      <Grid m={0} px={10} pb="xl" pt="xl">
+        <Grid.Col xs={12} lg={12}>
+          <div style={{ display: "flex", color: "grey" }}>
+            <Title mb="md">Appointment</Title>
+          </div>
+        </Grid.Col>
+      </Grid>
 
-            <Grid.Col xs={6} lg={6}>
-              <TextInput
-                radius="md"
-                placeholder="Email"
-                {...form.getInputProps("email")}
-              />
-            </Grid.Col>
-            <Grid.Col xs={6} lg={6} style={{}}>
-              <DatePickerInput
-                placeholder="Date of Birth"
-                {...form.getInputProps("dob")}
-                icon={<IconCalendar />}
-              />
-              
-            </Grid.Col>
+      <form onSubmit={(e) => onSubmit(e)}>
+        <Grid pt="xl" pb="xl" m={0} px={40}>
+          <Grid.Col xs={12} lg={12} pb="xl">
+            <TextInput
+              value={issue}
+              name="issue"
+              onChange={onInputChange}
+              radius="md"
+              placeholder="Issue"
+            />
+          </Grid.Col>
 
-            <Grid.Col xs={4} lg={4}>
-              <Select
-                placeholder="Gender"
-                {...form.getInputProps("gender")}
-                radius="md"
-                data={[
-                  { value: "male", label: "Male" },
-                  { value: "female", label: "Female" },
-                  { value: "other", label: "Other" },
-                ]}
-              />
-            </Grid.Col>
-            <Grid.Col xs={4} lg={4}>
-              <DatePickerInput
-                radius="md"
-                placeholder="Appointment Date"
-                {...form.getInputProps("date")}
-                icon={<IconCalendar />}
-                mx="auto"
-              />
-            </Grid.Col>
-            <Grid.Col xs={4} lg={4}>
-              <TimeInput
-                radius="md"
-                icon={<IconClock2 />}
-                placeholder="Appointment Time"
-                {...form.getInputProps("time")}
-              />
-            </Grid.Col>
+          <Grid.Col xs={6} lg={6} pb="xl">
+            <TextInput
+              value={patientId}
+              name="patientId"
+              disabled
+              radius="md"
+              placeholder="Patient Id"
+            />
+          </Grid.Col>
 
-            <Grid.Col xs={6} lg={6}>
-              <Select
-                placeholder="Department Name"
-                {...form.getInputProps("dept")}
-                radius="md"
-                data={[
-                  { value: "x", label: "X" },
-                  { value: "y", label: "Y" },
-                  { value: "z", label: "Z" },
-                ]}
-              />
-            </Grid.Col>
-            <Grid.Col xs={6} lg={6}>
-              <Select
-                placeholder="Doctor Name"
-                {...form.getInputProps("doctor")}
-                radius="md"
-                data={[
-                  { value: "x", label: "X" },
-                  { value: "y", label: "Y" },
-                  { value: "z", label: "Z" },
-                ]}
-              />
-            </Grid.Col>
+          <Grid.Col xs={6} lg={6} pb="xl">
+            
+            
+            <Select
+              value={doctorId}
+              name="doctorId"
+              onChange={(value) =>
+                onInputChange({ target: { name: "doctorId", value } })
+              }
+              placeholder="Doctor Id"
+              radius="md"
+              data={[
+                { value: "1", label: "1" },
+                { value: "2", label: "2" },
+                { value: "3", label: "3" },
+              ]}
+            />
+          </Grid.Col>
+          <Grid.Col xs={6} lg={6} pt="xl" pb="xl">
+            <DatePickerInput
+              value={date}
+              name="date"
+              onChange={(value) =>
+                onInputChange({ target: { name: "date", value } })
+              }
+              radius="md"
+              placeholder="Date"
+              icon={<IconCalendar />}
+              mx="auto"
+            />
+          </Grid.Col>
+          {/* <Grid.Col xs={6} lg={6} pt="xl" pb="xl">
+            <TimeInput
+              value={time}
+              name="time"
+              onChange={(value) =>
+                onInputChange({ target: { name: "time", value } })
+              }
+              radius="md"
+              icon={<IconClock2 />}
+              placeholder="Time"
+            />
+          </Grid.Col> */}
+          <Grid.Col xs={6} lg={6} pt="xl" pb="xl">
+             <TimeInput
+               value={time}
+               name="time"
+               onChange={(e) => onInputChange(e)}
+               radius="md"
+               icon={<IconClock2 />}
+               placeholder="Time"
+               // {...form.getInputProps("time")}
+             />
+           </Grid.Col>
 
-            <Grid.Col xs={12} lg={12}>
-              <Textarea
+          
+
+          <Grid.Col
+            xs={12}
+            lg={12}
+            pt="lg"
+            m={0}
+            px={0}
+            style={{ paddingTop: "35px", paddingBottom: "40px" }}
+          >
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                type="submit"
                 radius="md"
-                placeholder="Address"
-                {...form.getInputProps("address")}
-              />
-            </Grid.Col>
-            <Grid.Col
-              xs={12}
-              lg={12}
-              pt="lg"
-              m={0}
-              px={0}
-              style={{ paddingTop: "35px", paddingBottom: "40px" }}
-            >
-              <div style={{ display: "flex", justifyContent: "center" }}>
-                <Button
-                  type="submit"
-                  radius="md"
-                  size="md"
-                  style={{ backgroundColor: "#9370DB" }}
-                >
-                  REGISTER
-                </Button>
-              </div>
-            </Grid.Col>
-          </Grid>
-        </form>
-      </Card>
-    </div>
+                size="md"
+                style={{ backgroundColor: "#9370DB" }}
+              >
+                REGISTER
+              </Button>
+            </div>
+          </Grid.Col>
+        </Grid>
+      </form>
+    </>
   );
 }
--- reactapp/src/screens/patient/appointments/index.js
+++ reactapp/src/screens/patient/appointments/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   Table,
@@ -12,294 +12,179 @@ import {
   ActionIcon
 } from "@mantine/core";
 import moment from "moment";
-import { IconSearch, IconPlus, IconEdit, IconTrash } from "@tabler/icons-react";
-import AppointmentModal from "./modals/appointmentModal";
+// import { IconSearch, IconPlus, IconEdit, IconTrash } from "@tabler/icons-react";
+import ModalForm from "../appointmentForm";
 import CustomTable from "../../../components/customTable";
 
+import { useDisclosure } from "@mantine/hooks";
+import { Modal } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+import { IconSearch, IconPlus, IconEdit, IconTrash } from "@tabler/icons-react";
+
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+
+// export default function PatientAppointment() {
+//   const [patients, setPatients] = useState([]);
+  
+//   const {id}=useParams()
+
+//   useEffect(() => {
+//     loadPatients();
+//   }, []); //will run only once when the page loads
+
+//   const loadPatients = async () => {
+//     const result = await axios.get("http://localhost:8080/api/appointment/");
+//     setPatients(result.data);
+//   };
+
+//   const deletePatient=async(id)=>{
+//     await axios.delete(`http://localhost:8080/api/appointment/${id}`)
+//     loadPatients()
+//   }
 
 export default function PatientAppointment() {
-  const[editData, setEditData]=useState({});
-  const[rowData, setRowData]=useState([
-    {
-      id:1,
-      name: "Sanga Chakrabarty",
-      mobile: "6289645805",
-      email: "sangyachakrabarty@gmail.com",
-      dob: "2023-06-27T18:30:00.000Z",
-      gender: "female",
-      date: "2023-06-26T18:30:00.000Z",
-      time: "05:34",
-      dept: "y",
-      doctor: "z",
-      address: "54+56/1/2/2 kashinath Chatterjee Lane, Howrah-2"
-  },
-  {
-    id:2,  
-    name: "Bibyaswan Chakrabarty",
-    mobile: "5689645805",
-    email: "sangyachakrabarty@gmail.com",
-    dob: "2023-06-27T18:30:00.000Z",
-    gender: "female",
-    date: "2023-06-26T18:30:00.000Z",
-    time: "05:34",
-    dept: "y",
-    doctor: "z",
-    address: "54+56/1/2/2 kashinath Chatterjee Lane, Howrah-2"
+  const [patients, setPatients] = useState([]);
+  const patientId = 8; // patient ID should be set over here
 
-  }
-  ]);
-  const [appModal, setAppModal] = useState(false);
-  const apps = [
-    {
-      userName: "Adison Madsen",
-      email: "adds@gmail.com",
-      mobile: 7638908765,
-      date: "13th June, 2023",
-      time: "12:30 PM",
-      doctor: "X",
-      dept: "Y",
-    },
-    {
-      userName: "Craig Troff",
-      email: "craigs@gmail.com",
-      mobile: 7638678765,
-      date: "17th June, 2023",
-      time: "1:30 PM",
-      doctor: "Z",
-      dept: "X",
-    },
-    {
-      userName: "Curt Wills",
-      email: "curtss@gmail.com",
-      mobile: 7635678765,
-      date: "10th July, 2023",
-      time: "2:30 PM",
-      doctor: "Y",
-      dept: "Z",
-    },
-  ];
+  useEffect(() => {
+    loadPatients();
+  }, []); // Fetch patients 
+
+  const loadPatients = async () => {
+    try {
+      const result = await axios.get("http://localhost:8080/api/appointment");
+      const filteredPatients = result.data.filter(
+        (patient) => patient.patientId === patientId
+      );
+      setPatients(filteredPatients);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deletePatient = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8080/api/appointment/${id}`);
+      loadPatients();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+  const [opened, { open, close }] = useDisclosure(false);
 
   const colDef = [
     {
-      accessor: "name",
-      title: "Name",
+      accessor: "id",
+      title: "Id",
       titleStyle: { color: "" },
       textAlignment: "center",
-      // render: (data) => {
-      //   console.log("data", data);
-      //   // return
-      //   return (
-      //     <Group position="center">
-      //       <Text>{data?.userName}</Text>
-      //     </Group>
-      //   );
-      // },
-    },
-
-    {
-      accessor: "email",
-      title: "Email",
-      textAlignment: "center",
-      // render: (data) => {
-      //     console.log("wdw", data, data?.status, data?.status === "Active");
-      //     return <Text>{data?.date}</Text>;
-      // },
-    },
-    {
-      accessor: "mobile",
-      title: "Mobile",
-      textAlignment: "center",
-      // render: (data) => (
-      //     <Group position="center">
-      //         <Text>{data?.description}</Text>
-      //     </Group>
-      // ),
     },
     // {
-    //   accessor: "dob",
-    //   title: "DOB",
+    //   accessor: "patientId",
+    //   title: "Patient Id",
+    //   titleStyle: { color: "" },
     //   textAlignment: "center",
-    //   render: (data) => {
-    //     console.log("data", data);
-    //     // return
-    //     let m=moment(data?.dob).format("DD-MM-YYYY")
-    //     return (
-    //       <Group position="center">
-    //         <Text>{m}</Text>
-    //       </Group>
-    //     );
-    //   },
     // },
+
     {
-      accessor: "gender",
-      title: "Gender",
+      accessor: "doctorId",
+      title: "Doctor Id",
       textAlignment: "center",
-      // render: (data) => (
-      //     <Group position="center">
-      //         <Text>{data?.next_appointment}</Text>
-      //     </Group>
-      // ),
     },
     {
       accessor: "date",
       title: "Date",
       textAlignment: "center",
-      render: (data) => {
-        console.log("data", data);
-        // return
-        let m=moment(data?.date).format("DD-MM-YYYY")
-        return (
-          <Group position="center">
-            <Text>{m}</Text>
-          </Group>
-        );
-      },
     },
+
     {
       accessor: "time",
       title: "Time",
       textAlignment: "center",
-      // render: (data) => {
-      //   console.log("data", data);
-      //   // return
-      //   let m=moment(data?.time).format("hh:mm a")
-      //   return (
-      //     <Group position="center">
-      //       <Text>{m}</Text>
-      //     </Group>
-      //   );
-      },
-      
-    
-    // {
-    //   accessor: "dept",
-    //   title: "Department",
-    //   textAlignment: "center",
-    //   // render: (data) => (
-    //   //     <Group position="center">
-    //   //         <Text>{data?.amount}</Text>
-    //   //     </Group>
-    //   // ),
-    // },
-    {
-      accessor: "doctor",
-      title: "Doctor",
-      textAlignment: "center",
-      // render: (data) => (
-      //     <Group position="center">
-      //         <Text>{data?.amount}</Text>
-      //     </Group>
-      // ),
     },
-    // {
-    //   accessor: "address",
-    //   title: "Address",
-    //   textAlignment: "center",
-    //   // render: (data) => (
-    //   //     <Group position="center">
-    //   //         <Text>{data?.amount}</Text>
-    //   //     </Group>
-    //   // ),
-    // },
+    {
+      accessor: "issue",
+      title: "Issue",
+      textAlignment: "center",
+    },
+    {
+      accessor: "appointmentStatus",
+      title: "Status",
+      textAlignment: "center",
+    },
 
+    
 
     {
-      accessor: 'actions',
+      accessor: "actions",
       title: <Text mr="xs">Row actions</Text>,
-      textAlignment: 'center',
-      render: (data) => (
-          <Group spacing={4} position="center" noWrap>
-              <ActionIcon color="blue" 
-              onClick={() =>{
-                 setEditData(data);
-                 setAppModal(true);
-                 }}
-              >
-                  <IconEdit size={16} />
-              </ActionIcon>
-              <ActionIcon color="red" 
-              // onClick={() => handleDelete(data)}
-              >
-                  <IconTrash size={16} />
-              </ActionIcon>
-          </Group>
-      ),
+      textAlignment: "center",
     },
   ];
-  console.log("check", rowData);
-  console.log("mom", moment("2023-06-26T18:30:00.000Z").format("DD-MM-YYYY"));
-
   return (
-    
     <>
-      <div>
-        <AppointmentModal
-          open={appModal}
-          close={() => {
-            setEditData({});
-            setAppModal(false);
-          }}
-          rowData={rowData}
-          setRowData={setRowData}
-          editData={editData}
-        />
-        <Card
-          radius="lg"
-          //   style={{
-          //     width: "1000px",
-          //     height: "455px",
-          //     margin: "0",
-          //     padding: "0",
-          //   }}
-        >
-          <Grid>
-            <Grid.Col xs={4} lg={4} />
-            <Grid.Col xs={8} lg={8}>
-              <Group position="apart">
-                <Title>Appointment</Title>
+      <Grid pt="md" pb="lg" m={0} >
+        <Grid.Col xs={4} lg={4} />
+        <Grid.Col xs={8} lg={8}>
+          <Group position="apart">
+            <Title>Appointment</Title>
 
-                <Button
-                  onClick={() => {
-                    setAppModal(true);
-                  }}
-                >
-                  <IconPlus />
-                </Button>
-              </Group>
-            </Grid.Col>
-          </Grid>
+            <Modal
+              opened={opened}
+              onClose={close}
+             
+              centered
+            >
+              <ModalForm onCloseModal={close} patientId={patientId} />
+            </Modal>
 
-          {/* </Grid>
-          <Grid> */}
+            <Button onClick={open}>
+              <IconPlus />
+            </Button>
+          </Group>
+        </Grid.Col>
+      </Grid>
 
-          <TextInput
-            my="md"
-            radius="md"
-            placeholder="Search Name, Date, Number..."
-            icon={<IconSearch />}
-            // value={searchValue}
-            // onChange={handleInputChange}
-            // onIconClick={handleIconClick}
-            // style={{ borderBlockColor: "transparent" }}
-          />
+      <Table>
+        <thead>
+          <tr>
+            {colDef.map((column, index) => (
+              <th key={index}>{column.title}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {patients.map((patient, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
 
-          <CustomTable coloumnDef={colDef} records={rowData} />
-          {/* <Table >
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Mobile Number</th>
-                <th>Appointment Date</th>
-                <th>Appointment Time</th>
-                <th>Doctor</th>
-                <th>Department</th>
-              </tr>
-            </thead>
-            <tbody>{rows}</tbody>
-          </Table> */}
-        </Card>
-      </div>
+              {/* <td>{patient.patientId}</td> */}
+              <td>{patient.doctorId}</td>
+              <td>{patient.date}</td>
+              <td>{patient.time}</td>
+              <td>{patient.issue}</td>
+              <td>{patient.appointmentStatus}</td>
+              {/* <td>{patient.doctor}</td> */}
+              <td>
+                <Group spacing={4} position="center" noWrap>
+                
+                  <ActionIcon disabled color="blue" onClick={open} to={`/editPatient/${patient.id}`}>
+                    <IconEdit size={16} />
+                  </ActionIcon>
+                  <ActionIcon disabled color="red" onClick={()=>{deletePatient(patient.id)}}>
+                    <IconTrash size={16} />
+                  </ActionIcon>
+                </Group>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </>
   );
-}
+}   
